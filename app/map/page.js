43ded8ce08726b3e4de92ac125734e859ecc1ee0
zrcoy <zhangruichaotyler@gmail.com>
2024-08-24 11:01:49 +0800

    feat: add geofences

--- app/map/page.js
+++ app/map/page.js
@@ -1,12 +1,24 @@
 'use client'
-import { useJsApiLoader, GoogleMap } from '@react-google-maps/api'
-import { Spin, Button } from 'antd'
+import { useJsApiLoader, GoogleMap, Polygon } from '@react-google-maps/api'
+import { Spin, Button, Modal, Input, Select } from 'antd'
 import { useState, useCallback, useEffect } from 'react'
 import './map.css'
 
 const MapPage = () => {
   const [center, setCenter] = useState({ lat: -3.745, lng: -38.523 })
   const [loadingLocation, setLoadingLocation] = useState(true)
+  const [geofences, setGeofences] = useState([])
+  const [drawing, setDrawing] = useState(false)
+  const [currentGeofence, setCurrentGeofence] = useState([])
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [geofenceProps, setGeofenceProps] = useState({
+    name: '',
+    borderColor: '#FF0000',
+    fillColor: '#FF0000',
+  })
+  console.log('geofences:', geofences)
+  console.log('currentGeofence:', currentGeofence)
+  console.log('geofenceProps:', geofenceProps)
 
   const containerStyle = {
     width: '100%',
@@ -22,7 +34,6 @@ const MapPage = () => {
 
   const onLoad = useCallback(
     function callback(map) {
-      // This is just an example of getting and using the map instance!!! don't just blindly copy!
       const bounds = new window.google.maps.LatLngBounds(center)
       map.fitBounds(bounds)
       setMap(map)
@@ -38,11 +49,6 @@ const MapPage = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          // setCenter({
-          //   lat: position.coords.latitude,
-          //   lng: position.coords.longitude,
-          // })
-          // TODO: set center back when finish
           setCenter({ lat: -3.745, lng: -38.523 })
           setLoadingLocation(false)
           console.log('User location:', position.coords)
@@ -58,10 +64,42 @@ const MapPage = () => {
     }
   }, [])
 
+  const handleMapClick = (event) => {
+    if (drawing) {
+      setCurrentGeofence([
+        ...currentGeofence,
+        { lat: event.latLng.lat(), lng: event.latLng.lng() },
+      ])
+    }
+  }
+
+  const handleAddGeofence = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleStartDrawing = () => {
+    setDrawing(true)
+    setIsModalVisible(false)
+  }
+
+  const handleCompleteGeofence = () => {
+    setGeofences([...geofences, { ...geofenceProps, path: currentGeofence }])
+    setCurrentGeofence([])
+    setDrawing(false)
+  }
+
+  const handleModalChange = (e) => {
+    const { name, value } = e.target
+    setGeofenceProps({ ...geofenceProps, [name]: value })
+  }
+
   return (
     <div className="map-container">
       <div className="actions-bar">
-        <Button type="primary">Add</Button>
+        <div style={{ fontSize: 30, marginRight: 30 }}>Geofences</div>
+        <Button type="primary" onClick={handleAddGeofence}>
+          Add
+        </Button>
         <Button type="primary">Delete</Button>
         <Button type="primary">Edit</Button>
       </div>
@@ -73,13 +111,63 @@ const MapPage = () => {
           zoom={10}
           onLoad={onLoad}
           onUnmount={onUnmount}
+          onClick={handleMapClick}
         >
-          {/* Child components, such as markers, info windows, etc. */}
+          {geofences.map((geofence, index) => (
+            <Polygon
+              key={index}
+              paths={geofence.path}
+              options={{
+                fillColor: geofence.fillColor,
+                fillOpacity: 0.4,
+                strokeColor: geofence.borderColor,
+                strokeOpacity: 1,
+                strokeWeight: 2,
+              }}
+            />
+          ))}
+          {drawing && (
+            <Polygon
+              paths={currentGeofence}
+              options={{
+                fillColor: geofenceProps.fillColor,
+                fillOpacity: 0.4,
+                strokeColor: geofenceProps.borderColor,
+                strokeOpacity: 1,
+                strokeWeight: 2,
+              }}
+            />
+          )}
         </GoogleMap>
       ) : (
         <div className="spinner">
           <Spin size="large" />
         </div>
+      )}
+
+      <Modal
+        title="Add Geofence"
+        open={isModalVisible}
+        onOk={handleStartDrawing}
+        onCancel={() => setIsModalVisible(false)}
+      >
+        <Input name="name" placeholder="Name" onChange={handleModalChange} />
+        <Input
+          name="borderColor"
+          placeholder="Border Color"
+          onChange={handleModalChange}
+        />
+        <Input
+          name="fillColor"
+          placeholder="Fill Color"
+          onChange={handleModalChange}
+        />
+      </Modal>
+
+      {drawing && (
+        <Button type="primary" onClick={handleCompleteGeofence}>
+          Complete
+        </Button>
       )}
     </div>
   )
